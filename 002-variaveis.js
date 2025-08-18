d86ada609d7d1874068835cd977897fd621f9d75
LUIZ HENRIQUE ANDRADE SANTOS <andradesantos132019@gmail.com>
2025-08-18 19:35:03 -0300

    Initial commit

---
+++ 002-variaveis.js
@@ -0,0 +1,18 @@
+let texto ="Hoje não está muito frio";
+console.log(texto);
+console.log("Novo valor a seguir: ");
+
+
+
+let numero = 10;
+
+let numero2 = 12.7;
+
+var titulotexto = "Como está o dia";
+
+const escola = "ETEC Prof. Massuyuki kawano";
+
+// escola = "ETEC Tupã"; = erro
+
+texto = "Hoje está frio";
+console.log(texto);
